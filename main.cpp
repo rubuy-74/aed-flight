6315bd7472346c0b52b671ed22d61b17d1f837a7
Mourinha08 <afonsomoura485@gmail.com>
2023-12-20 12:39:29 +0000

    Change on main.cpp

--- main.cpp
+++ main.cpp
@@ -17,18 +17,5 @@ int main() {
 
     Dataset dataset = Dataset(rawFlights, rawAirports, rawAirlines);
 
-    Airport airport = Airport("CDG","Charles De Gaulle","Paris","France",Coordinate(49.012779,2.550000));
-
-    Functions functions;
-    cout << functions.getNumFlightsOutOfAnAirport(airport) << '\n';
-    cout << functions.getNumAirlinesOfAnAirport(airport) << '\n';
-    cout << functions.getNumDestinationsAirportsOfAnAirport(airport) << '\n';
-    cout << functions.getNumDestinationsCountriesOfAnAirport(airport) << '\n';
-    cout << functions.getNumDestinationsCitiesOfAnAirport(airport) << '\n';
-    vector<string> topK = functions.topKAirports(7);
-    for(string s : topK){
-        cout << s << '\n';
-    }
-
     return 0;
 }
