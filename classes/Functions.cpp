3b5f7192b02f03843e86b68238b8431e63814abb
Mourinha08 <afonsomoura485@gmail.com>
2023-12-20 12:26:48 +0000

    included dataset in Functions class

--- classes/Functions.cpp
+++ classes/Functions.cpp
@@ -7,8 +7,8 @@
 #include "algorithm"
 #include "vector"
 
-unsigned int Functions::getNumFlightsOutOfAnAirport(Graph g, Airport airport) {
-    return g.findAirport(airport)->getFlights().size();
+unsigned int Functions::getNumFlightsOutOfAnAirport(Airport airport) {
+    return dataset.getNetwork().findAirport(airport)->getFlights().size();
 }
 
 bool Functions::findInVector(const vector<string>& v, const string& x){
@@ -18,10 +18,10 @@ bool Functions::findInVector(const vector<string>& v, const string& x){
     return false;
 }
 
-int Functions::getNumAirlinesOfAnAirport(Graph g, Airport airport) {
+int Functions::getNumAirlinesOfAnAirport(Airport airport) {
     vector<string> v;
     int count = 0;
-    for(auto flight : g.findAirport(airport)->getFlights()){
+    for(auto flight : dataset.getNetwork().findAirport(airport)->getFlights()){
         if(!findInVector(v, flight->getAirline().getCode())){
             v.push_back(flight->getAirline().getCode());
             count++;
@@ -30,10 +30,10 @@ int Functions::getNumAirlinesOfAnAirport(Graph g, Airport airport) {
     return count;
 }
 
-int Functions::getNumDestinationsAirportsOfAnAirport(Graph g, Airport airport) {
+int Functions::getNumDestinationsAirportsOfAnAirport(Airport airport) {
     vector<string> v;
     int count = 0;
-    for(auto flight : g.findAirport(airport)->getFlights()){
+    for(auto flight : dataset.getNetwork().findAirport(airport)->getFlights()){
         if(!findInVector(v,flight->getDestination()->getCode())){
             v.push_back(flight->getDestination()->getCode());
             count++;
@@ -42,10 +42,10 @@ int Functions::getNumDestinationsAirportsOfAnAirport(Graph g, Airport airport) {
     return count;
 }
 
-int Functions::getNumDestinationsCitiesOfAnAirport(Graph g, Airport airport) {
+int Functions::getNumDestinationsCitiesOfAnAirport(Airport airport) {
     vector<string> v;
     int count = 0;
-    for(auto flight : g.findAirport(airport)->getFlights()){
+    for(auto flight : dataset.getNetwork().findAirport(airport)->getFlights()){
         if(!findInVector(v,flight->getDestination()->getCity())){
             v.push_back(flight->getDestination()->getCity());
             count++;
@@ -54,10 +54,10 @@ int Functions::getNumDestinationsCitiesOfAnAirport(Graph g, Airport airport) {
     return count;
 }
 
-int Functions::getNumDestinationsCountriesOfAnAirport(Graph g, Airport airport) {
+int Functions::getNumDestinationsCountriesOfAnAirport(Airport airport) {
     vector<string> v;
     int count = 0;
-    for(auto flight : g.findAirport(airport)->getFlights()){
+    for(auto flight : dataset.getNetwork().findAirport(airport)->getFlights()){
         if(!findInVector(v,flight->getDestination()->getCountry())){
             v.push_back(flight->getDestination()->getCountry());
             count++;
@@ -66,10 +66,10 @@ int Functions::getNumDestinationsCountriesOfAnAirport(Graph g, Airport airport) 
     return count;
 }
 
-vector<string> Functions::topKAirports(Graph g, int k) {
+vector<string> Functions::topKAirports(int k) {
     vector<string> res;
     vector<Airport*> tmp;
-    for(auto airport : g.getAirports()){
+    for(auto airport : dataset.getNetwork().getAirports()){
         tmp.push_back(airport.second);
     }
     sort(tmp.begin(),tmp.end(),[](Airport* a1, Airport* a2){
@@ -79,4 +79,53 @@ vector<string> Functions::topKAirports(Graph g, int k) {
         res.push_back(tmp[i]->getName());
     }
     return res;
+}
+
+unordered_map<string, int> Functions::getFlightsPerCity() {
+    unordered_map<string,int> flightsPerCity;
+    for(auto airports: dataset.getNetwork().getAirports()){
+        for(auto flight : airports.second->getFlights()){
+            flightsPerCity[airports.second->getCity()]++;
+            flightsPerCity[flight->getDestination()->getCity()]++;
+        }
+    }
+    return flightsPerCity;
+}
+
+unordered_map<string, int> Functions::getFlightsPerAirline() {
+    unordered_map<string,int> flightsPerAirline;
+    for(auto airports: dataset.getNetwork().getAirports()){
+        for(auto flight:airports.second->getFlights()){
+            flightsPerAirline[flight->getAirline().getName()]++;
+        }
+    }
+    return flightsPerAirline;
+}
+
+int Functions::getNumAirportsAtDistance(Airport airport, int distance) {
+    return dataset.getNetwork().bfsAtDistance(dataset.getNetwork().findAirport(airport),distance).size();
+}
+
+int Functions::getNumCititesAtDistance(Airport airport, int distance) {
+    auto destinations = dataset.getNetwork().bfsAtDistance(dataset.getNetwork().findAirport(airport),distance);
+    vector<string> cities;
+    for(auto a : destinations){
+        if(find(cities.begin(),cities.end(),a->getCity()) != cities.end())
+            cities.push_back(a->getCity());
+    }
+    return cities.size();
+}
+
+int Functions::getNumCountriesAtDistance(Airport airport, int distance) {
+    auto destinations = dataset.getNetwork().bfsAtDistance(dataset.getNetwork().findAirport(airport),distance);
+    vector<string> countries;
+    for(auto a: destinations){
+        if(find(countries.begin(),countries.end(),a->getCountry()) != countries.end())
+            countries.push_back(a->getCountry());
+    }
+    return countries.size();
+}
+
+unordered_set<Airport *> Functions::getArticulationPoints() {
+    return dataset.getNetwork().getArticulationPoints();
 }
--- main.cpp
+++ main.cpp
@@ -14,5 +14,21 @@ int main() {
     list<vector<string>> rawFlights = parser.readFile("../dataset/flights.csv");
     list<vector<string>> rawAirlines = parser.readFile("../dataset/airlines.csv");
     list<vector<string>> rawAirports = parser.readFile("../dataset/airports.csv");
+
+    Dataset dataset = Dataset(rawFlights, rawAirports, rawAirlines);
+
+    Airport airport = Airport("CDG","Charles De Gaulle","Paris","France",Coordinate(49.012779,2.550000));
+
+    Functions functions;
+    cout << functions.getNumFlightsOutOfAnAirport(airport) << '\n';
+    cout << functions.getNumAirlinesOfAnAirport(airport) << '\n';
+    cout << functions.getNumDestinationsAirportsOfAnAirport(airport) << '\n';
+    cout << functions.getNumDestinationsCountriesOfAnAirport(airport) << '\n';
+    cout << functions.getNumDestinationsCitiesOfAnAirport(airport) << '\n';
+    vector<string> topK = functions.topKAirports(7);
+    for(string s : topK){
+        cout << s << '\n';
+    }
+
     return 0;
 }
